26dbbd20aa2f3ef7b32ad135cc744eb3d971b702
Fuzail Khan <fuzailxkhan@gmail.com>
2024-03-10 13:21:46 +0500

    Initialized the Project and installed React Router Dom

---
+++ Client/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1 className='underline bg-blue-500'>Fuzail</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <div>
+        <nav className="bg-gray-500 min-h-10">
+            <div>Logo</div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
