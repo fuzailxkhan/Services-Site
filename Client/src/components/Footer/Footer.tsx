f50497f11eac7e2f3fe5ee0f08ab895be364fee1
Fuzail Khan <fuzailxkhan@gmail.com>
2024-03-11 11:30:35 +0500

    Footer almost done

--- Client/src/components/Footer/Footer.tsx
+++ Client/src/components/Footer/Footer.tsx
@@ -3,21 +3,35 @@ import shipLogo from '../Header/navbar_logo.jpg';
 
 const Footer = () => {
   return (
-    <footer className=" p-2">
-      <div className="border  rounded-md shadow-md shadow-gray-400 p-2">
+    <footer className="p-2">
+      <div className="border  rounded-md shadow-md shadow-gray-400 p-2 pt-5">
 
-        <div className="border-b grid grid-cols-2 sm:ms-52 sm:me-52">
+        <div className="border-b grid grid-cols-1 sm:grid-cols-2 sm:ms-52 sm:me-52 pb-5 pt-3">
 
-            <div className="justify-self-start">
+            <div className="justify-self-center sm:block hidden">
               <img className='max-h-28' src={shipLogo} ></img>
             </div>
 
-            <div className="justify-self-start">
+            <div className="justify-self-center sm:flex gap-10 sm:gap-20">
 
-              <div>Fuzail</div>
-              <div>Fuzail</div>
-              <div>Fuzail</div>
-              
+              <div className='border-s ps-5'>
+                <p className='font-semibold pb-2'>Resources</p>
+                <p className='text-gray-500'>About</p>
+                <p className='text-gray-500'>Home</p>
+              </div>
+
+              <div className='border-s ps-5'>
+                <p className='font-semibold pb-2'>Follow Us</p>
+                <p className='text-gray-500'>Facebook</p>
+                <p className='text-gray-500'>Twitter</p>
+              </div>
+
+              <div className='border-s ps-5'>
+                <p className='font-semibold pb-2'>Legal</p>
+                <p className='text-gray-500'>Privacy Policy</p>
+                <p className='text-gray-500'>Terms & Condition</p>
+              </div>
+
             </div>
 
         </div>
