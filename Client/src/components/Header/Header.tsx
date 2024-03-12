9d65deaf0fac83052d5b740c9541c87c72ebe37d
Fuzail Khan <fuzailxkhan@gmail.com>
2024-03-12 16:10:13 +0500

    Login Page

--- Client/src/components/Header/Header.tsx
+++ Client/src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className="min-h-10 grid grid-cols-2 p-2 m-2 rounded-lg sm:grid-cols-3 shadow-xl border mb-2">
 
             <div className="col-span-1 justify-self-center  self-center hidden sm:block">
-                <Link to="/" className='flex'><img className="max-h-10 m-0" src={shipLogo} /><p className='self-center ms-5 font-semibold text-blue-900'>World Wide Shipping</p></Link>
+                <Link to="/" className='flex'><img className="max-h-10 m-0" src={shipLogo} /><p className='self-center ms-5 font-semibold text-blue-900 hover:text-black'>World Wide Shipping</p></Link>
             </div>
 
             <div className='self-center'>
@@ -26,7 +26,7 @@ const Header = () => {
             
 
             <div className='grid grid-cols-2 col-span-1 justify-self-end sm:justify-self-center self-center'>
-                <p className='self-center justify-self-center font-semibold text-blue-700 hover:text-orange-500'><NavLink className={({isActive})=>`${isActive&&"border-b-4 border-blue-500 pb-3.5 px-2"}`} to="/Login">Login</NavLink></p>
+                <p className='self-center justify-self-center font-semibold text-blue-700 hover:text-black'><NavLink className={({isActive})=>`${isActive&&"border-b-4 border-blue-500 pb-3.5 px-2"}`} to="/Login">Login</NavLink></p>
                 <button className=" bg-blue-500 text-white ps-4 pe-4 p-1.5 rounded-md shadow-md shadow-gray-400  hover:bg-blue-600">
                     <Link to="/Contact">Sign Up</Link>
                 </button>
