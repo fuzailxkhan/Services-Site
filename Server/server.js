18ea8848b69e925ad24d1aedd02f0bcf6d77be75
Fuzail Khan <fuzailxkhan@gmail.com>
2024-04-27 02:58:20 -0700

    using bcrypt while saving passwords

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs')
+const fs = require('fs');
 const cloudinary = require('cloudinary').v2;
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.json({ limit: '6mb' }));
 app.use(cors());
 app.use(express.urlencoded({extended: true }));
+
+const userSchema = require('./schema');
+mongoose.connect('mongodb+srv://fuzail:spiderman123@cluster0.1ig1y1c.mongodb.net/?retryWrites=true&w=majority')
+const db = mongoose.connection;
 
 app.listen(3000,(err)=>{
     if(err) throw err;
@@ -19,7 +25,14 @@ cloudinary.config({
   api_secret: '9nTu4HMZRoE7psw2hDCy76dmpkA' 
 });
 
+const emailFind = async (email)=>{
+    const user = await userSchema.findOne({email:email});
+    return user
+}
+
 app.post('/SignUp',async (req,res)=>{
+
+
     
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
@@ -28,6 +41,9 @@ app.post('/SignUp',async (req,res)=>{
     console.log(firstName,lastName,email,password);
     const pic = req.body.picture;
 
+    const user = await emailFind(email);
+    if(user){return res.json({"Message":"User already exists"})}
+    console.log('email not found')
     const matches = pic.match(/^data:image\/([A-Za-z-+\/]+);base64,(.+)$/);
     if (!matches) {
         throw new Error('Invalid base64 image string');
@@ -37,9 +53,13 @@ app.post('/SignUp',async (req,res)=>{
     const base64Data = matches[2];
 
     const result = await cloudinary.uploader.upload(`data:image/${imageType};base64,${base64Data}`);
-
     const imageUrl = result.secure_url;
-    console.log(imageUrl)
-    res.send(imageUrl)
+
+    const pass = await bcrypt.hash(password,10);
+
+    const newUser = userSchema({fname:firstName,lname:lastName,email:email,password:pass,pfp:imageUrl})
+    const savedUser = await newUser.save();
+    res.status(200).json({"Message":"New Account Created on "+newUser.email});
+    
     
 })
